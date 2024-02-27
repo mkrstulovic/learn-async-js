2f473f6d6cefe84a1fbb1df83b4a37e7f03a2c50
Marko Krstulovic <mk366d@outlook.com>
2024-02-27 17:00:34 -0500

    Completed class activities for February 27, 2024

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,37 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function arrayHasNeg(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('FindNeg called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let negativesInArray = arr.filter(num => num < 0);
+                for (let i = 0; i < arr.length; i++) {
+                    if (arr[i] < 0) {
+                        console.log('resolving ... ');
+                        found = true;
+                        resolve(arr);
+                    }
+                }
+                if (negativesInArray.length > 0) {
+                    console.log('resolving ... ');
+                    resolve(arr);
+                } else {
+                    console.log('rejecting ... ');
+                    reject("No negative");
+                }
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from findNeg');
+    });
+}
+
+const findNegPromise = Promise.any(array2D.map(row => arrayHasNeg(row)))
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,34 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+function sumArray(arr) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr.length; i++) {
+                    sum += arr[i];
+                }
+                console.log('resolving ... ');
+                resolve(sum);
+            }, 0);
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+        console.log('returning from sum');
+    });
+}
+
+const sumPromise = Promise.all(array2D.map(row => sumArray(row)))
+  .then((responses) => {
+      let totalSum = 0;
+      for (const response of responses) {
+          totalSum += response;
+      }
+      console.log(totalSum);
+    })
+.catch((err) => console.log(err));
